1ca076df753bae0b4869d1b0037824a84c2bb18c
JamieCorkhill <jamie.corkhill@Live.com>
2019-12-16 17:55:50 -0600

    Fix indentation

--- src/common/controllers/BaseHTTPController.ts
+++ src/common/controllers/BaseHTTPController.ts
@@ -9,68 +9,68 @@ import { HTTP } from './../interfaces/interfaces';
  * Contains helper methods for HTTP Responses
  */
 export class BaseHTTPController {
-	public constructor (private readonly httpContext: HTTP.IHttpContext) {}
+    public constructor (private readonly httpContext: HTTP.IHttpContext) {}
 
-	//#region Success (2xx) Response Helpers
+    //#region Success (2xx) Response Helpers
 
-	/**
-	 * Responds with status code 200 and the provided JSON to the HTTP Client.
-	 * @param json JSON Response Payload.
-	 */
-	protected ok(json?: any): Response {
-		return this.json(json, HttpStatus.OK);
-	}
+    /**
+     * Responds with status code 200 and the provided JSON to the HTTP Client.
+     * @param json JSON Response Payload.
+     */
+    protected ok(json?: any): Response {
+        return this.json(json, HttpStatus.OK);
+    }
 
-	/**
-	 * Responds with status code 200 and the provided DTO to the HTTP Client.
-	 * @param dto Response DTO
-	 */
-	protected withDTO<T>(dto: T): Response {
-		return this.ok(dto);
-	}
+    /**
+     * Responds with status code 200 and the provided DTO to the HTTP Client.
+     * @param dto Response DTO
+     */
+    protected withDTO<T>(dto: T): Response {
+        return this.ok(dto);
+    }
 
-	/**
-	 * Responds with a status code 201 and the provided JSON to the HTTP Client.
-	 * @param json JSON Response Payload
-	 */
-	protected createdOk(json?: any): Response {
-		return this.json(json, HttpStatus.OK);
-	}
+    /**
+     * Responds with a status code 201 and the provided JSON to the HTTP Client.
+     * @param json JSON Response Payload
+     */
+    protected createdOk(json?: any): Response {
+        return this.json(json, HttpStatus.OK);
+    }
 
-	//#endregion
+    //#endregion
 
-	//#region Client Error (4xx) Response Helpers
+    //#region Client Error (4xx) Response Helpers
 
-	/**
-	 * Responds with a status code 400 and the provided error to the HTTP Client.
-	 * @param error The error message.
-	 */
-	protected clientError(error: any): Response {
-		return this.json(error, HttpStatus.BAD_REQUEST);
-	}
+    /**
+     * Responds with a status code 400 and the provided error to the HTTP Client.
+     * @param error The error message.
+     */
+    protected clientError(error: any): Response {
+        return this.json(error, HttpStatus.BAD_REQUEST);
+    }
 
-	//#endregion
+    //#endregion
 
-	//#region Failure Errors
+    //#region Failure Errors
 
-	/**
-	 * Responds with a status code 500 and the provided JSON to the HTTP client.
-	 * @param error Error response payload
-	 */
-	protected fail(error?: any): Response {
-		return this.json(error, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+    /**
+     * Responds with a status code 500 and the provided JSON to the HTTP client.
+     * @param error Error response payload
+     */
+    protected fail(error?: any): Response {
+        return this.json(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-	//#endregion
+    //#endregion
 
-	//#region Miscellaneous JSON Response Helpers
+    //#region Miscellaneous JSON Response Helpers
 
-	/**
-	 * Responds with the provided JSON and status code to the HTTP Client.
-	 * @param json 		 JSON Response Payload
-	 * @param statusCode HTTP Response Status Code
-	 */
-	protected json(json: any, statusCode = HttpStatus.OK): Response {
-		return this.httpContext.res.status(statusCode).send(json);
-	}
+    /**
+     * Responds with the provided JSON and status code to the HTTP Client.
+     * @param json 		 JSON Response Payload
+     * @param statusCode HTTP Response Status Code
+     */
+    protected json(json: any, statusCode = HttpStatus.OK): Response {
+        return this.httpContext.res.status(statusCode).send(json);
+    }
 }
