4085ecf604d25cc24103b0f77b96757b7f7b56c8
JamieCorkhill <jamie.corkhill@Live.com>
2019-12-16 17:54:21 -0600

    Fix Base Controller formatting/indentation

--- src/common/controllers/BaseHTTPController.ts
+++ src/common/controllers/BaseHTTPController.ts
@@ -11,16 +11,16 @@ import { HTTP } from './../interfaces/interfaces';
 export class BaseHTTPController {
 	public constructor (private readonly httpContext: HTTP.IHttpContext) {}
 
-    //#region Success (2xx) Response Helpers
+	//#region Success (2xx) Response Helpers
 
-     /**
-      * Responds with status code 200 and the provided JSON to the HTTP Client.
-      * @param json JSON Response Payload.
-      */
-    protected ok(json?: any): Response {
+	/**
+	 * Responds with status code 200 and the provided JSON to the HTTP Client.
+	 * @param json JSON Response Payload.
+	 */
+	protected ok(json?: any): Response {
 		return this.json(json, HttpStatus.OK);
 	}
-	
+
 	/**
 	 * Responds with status code 200 and the provided DTO to the HTTP Client.
 	 * @param dto Response DTO
@@ -46,25 +46,25 @@ export class BaseHTTPController {
 	 * @param error The error message.
 	 */
 	protected clientError(error: any): Response {
-        return this.json(error, HttpStatus.BAD_REQUEST);
+		return this.json(error, HttpStatus.BAD_REQUEST);
 	}
-	
+
 	//#endregion
 
 	//#region Failure Errors
 
-    /**
-     * Responds with a status code 500 and the provided JSON to the HTTP client.
-     * @param error Error response payload
-     */
-    protected fail(error?: any): Response {
-        return this.json(error, HttpStatus.INTERNAL_SERVER_ERROR);
+	/**
+	 * Responds with a status code 500 and the provided JSON to the HTTP client.
+	 * @param error Error response payload
+	 */
+	protected fail(error?: any): Response {
+		return this.json(error, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	
-    //#endregion
+
+	//#endregion
 
 	//#region Miscellaneous JSON Response Helpers
-	
+
 	/**
 	 * Responds with the provided JSON and status code to the HTTP Client.
 	 * @param json 		 JSON Response Payload
