21fe0fd08dd8e50caf4f528e13a4fac9eacbc8df
JamieCorkhill <jamie.corkhill@Live.com>
2019-12-19 18:01:47 -0600

    Remove build

--- build/features/users/models/domain/user.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ulid_1 = require("ulid");
-exports.buildUser = (userProps, id) => {
-    return Object.assign({ id: id ? id : ulid_1.ulid() }, userProps);
-};
